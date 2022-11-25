85ccf0c575941b4290aa511f119d814f600ff062
BuffManKyle <epsolfwd@gmail.com>
2022-11-24 20:10:30 -0500

    switch back to desktop

--- src/pages/partials/contactform.js
+++ src/pages/partials/contactform.js
@@ -1,12 +1,40 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './form.css';
-import { Col, Button, Row, Container} from 'react-bootstrap';
+import { Col, Button, Row, Container } from 'react-bootstrap';
 
 function Contactform () {
     return (
         <>
-            <h3>Contact form here</h3>
-            <p>seperate component from home.js</p>
+            <Container>
+                <form className="contactForm">
+                    <Col>
+                        <input name="name" type="text" placeholder="Name"></input>
+                    </Col>
+                    <Col>
+                        <input name="email" type="email" placeholder="Email"></input>
+                    </Col>
+                    <Col>
+                        <input name="phone" type="tel" placeholder="Phone Number"></input>
+                    </Col>
+                    <Col>
+                        <select id="service" name="service" >
+                            <option value="protectiveCoating" >Protective Coating</option>
+                            <option value="paintEnhancment" >Coating + Paint Enhancment</option>
+                            <option value="one" >Coating + One Stage Correction</option>
+                            <option value="multi" >Coating + Multistage Correction</option>
+                            <option value="addon" >Coating Add-Ons</option>
+                           </select>
+                    </Col>
+                    <Col>
+                        <textarea name="message" placeholder="Message"></textarea>
+                    </Col>
+                    
+                    
+                    
+                </form>
+
+
+            </Container>
         </>
     );
 };
