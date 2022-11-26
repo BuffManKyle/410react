5113ccab5ac86cd110c1df1a7dcfbd1b1288f6ca
BuffManKyle <epsolfwd@gmail.com>
2022-11-25 19:20:49 -0500

    img alt

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,7 +16,7 @@ function About () {
                 </Row>
                 <Row className="content-spacing">
                     <Col lg={{ span: 5, offset: 1 }}>
-                        <img src={Aboutimg} className="d-block w-75 aboutimg"/>
+                        <img src={Aboutimg} className="d-block w-75 aboutimg" alt="aboutimg"/>
                     </Col>
                     <Col lg={{ span: 5, offset: 0 }} className="content-block p-3" >
                         <h5>Four One O' Detailing is a professional mobile and on-site shop vehicle detailing service created in March of 2020. Born from a love of all things automotive, I strive to deliver an extraordinary detailing experience that meets all of my customers' expectations. The same attention to detail I put into conducting a paint correction and coating is the same focus I apply to customer communication and honest expectations. Family values, hard work, and dedication drive the vision I set forth to create a vehicle detailing service that offers honest prices and superb results.
