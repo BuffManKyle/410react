d853fc9af85cea4d1e73c6f26ee949e6b89ac809
BuffManKyle <epsolfwd@gmail.com>
2022-11-27 20:03:21 -0500

    color change, remove about page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,28 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles.css"
-import { Container, Col, Row } from 'react-bootstrap';
+import "./styles/about.css";
+import { Container } from 'react-bootstrap';
+
 
 
 function About () {
 
 
     return (
-        <>
+        <>  
             <Container fluid>
 
-                <Row className="content-spacing justify-content-evenly">
-                    
-                    <Col lg={5} className="about-block">
-                        <h3>About Us</h3>
-                        <h5>Four One O' Detailing is a professional mobile and on-site shop vehicle detailing service created in March of 2020. Born from a love of all things automotive, I strive to deliver an extraordinary detailing experience that meets all of my customers' expectations. The same attention to detail I put into conducting a paint correction and coating is the same focus I apply to customer communication and honest expectations. Family values, hard work, and dedication drive the vision I set forth to create a vehicle detailing service that offers honest prices and superb results.  
-                        </h5>
-                        <h4 className="align-right"> - Cody, Owner</h4>
-                    </Col>
 
-
-                    
-                </Row>
+                
             </Container>
+            
+            
+            
         </>
     )
 };
--- src/pages/single.js
+++ src/pages/single.js
@@ -1,13 +1,32 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css"
-import { Container } from 'react-bootstrap';
+import "./styles/single.css";
+import Onestageimg from './assets/images/314789663_670013071295906_1018968343675313872_n.jpg'
 
 function Single () {
     return (
         <>
-            <Container>
+            
+                    <div>
+                        <h1 className="oneStageTitle">One Stage Paint Correction</h1>
+                    </div>
+                    <div  className="oneStageImgWrapper">
+                        <img className="oneStageImg"
+                        src={Onestageimg}
+                        alt="Mercades hood"
+                    />
+                    </div>
 
-            </Container>
+                    <div className="oneStageContent">
+                        <p>          
+                            Shower foam hand wash and a two-bucket method to start. Vehicle is stripped of environmental fallout and contaminants using an iron fallout remover and a hand applied clay bar process completed.  Paint is then polished using a DA polisher and 65-75% of paint defects removed.  Deeper scratches and heavy abrasions will remain, but will be severely less noticeable.  A ceramic wax is then applied to the paint to provide extreme water and UV protection [up to nine (9) month durability].  Wheels, tires, and wheel wells are cleaned and a gel tire shine is applied to retain tire sheen with no tire sling.
+                        </p>
+                        <p>
+                            The best protection for a one stage paint correction is a coating.  Let us know your needs and we can get you setup with the appropriate package!
+                        </p>  
+                    </div>
+                
+                
         </>
     )
 };
