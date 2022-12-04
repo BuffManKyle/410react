22fbd0616088d16a959473fd624e39958d38750a
BuffManKyle <epsolfwd@gmail.com>
2022-12-04 18:34:41 -0500

    not scrolling on render fix

--- src/helpers/ScrollToTop.js
+++ src/helpers/ScrollToTop.js
@@ -1,17 +1,17 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-export default function ScrollToTop({ children }) {
+export default function ScrollToTop() {
   const { pathname } = useLocation();
-  
-  useEffect(() => {
-    const canControlScrollRestoration = 'scrollRestoration' in window.history
-    if (canControlScrollRestoration) {
-      window.history.scrollRestoration = 'manual';
-    }
 
-    window.scrollTo(0, 0);
+  useEffect(() => {
+    // "document.documentElement.scrollTo" is the magic for React Router Dom v6
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant", // Optional if you want to skip the scrolling animation
+    });
   }, [pathname]);
-  
-  return children;
+
+  return null;
 }
--- src/index.js
+++ src/index.js
@@ -10,8 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <ScrollToTop />
+    <ScrollToTop>
     <App />
+    </ScrollToTop>
     </BrowserRouter>
   </React.StrictMode>
 );
